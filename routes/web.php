d8bfbdbaa1b70b74068d9836c9d692c8c3ef2e7b
Jiyoung Choi <b00158376@mytudublin.ie>
2025-04-14 21:15:50 +0100

    route/web.php updated

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\TrainerController;
+use App\Http\Controllers\GymClassController;
+use App\Http\Controllers\BookingController;
+use App\Http\Controllers\MembershipController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +21,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('users', UserController::class);
+Route::resource('trainers', TrainerController::class);
+Route::resource('classes', GymClassController::class);
+Route::resource('bookings', BookingController::class);
+Route::resource('memberships', MembershipController::class);
