2c53bfa93a5917aacdc1a497c71047e5e8978c46
Jiyoung Choi <b00158376@mytudublin.ie>
2025-04-25 15:48:30 +0100

    Booking page changed

--- resources/views/bookings/index.blade.php
+++ resources/views/bookings/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="d-flex justify-content-between align-items-center mb-3">
 <h1 class="mb-4 fw-bold display-5">Class Booking</h1>
-    <a href="{{ route('memberships.create') }}" class="btn btn-primary">
+    <a href="{{ route('bookings.create') }}" class="btn btn-primary">
         <i class="fas fa-plus"></i> New Booking
     </a>
 </div>
