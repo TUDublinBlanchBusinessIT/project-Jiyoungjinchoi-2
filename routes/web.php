ae5cb0f99edacd97deefcfeaffa563c568e77fdb
Jiyoung Choi <b00158376@mytudublin.ie>
2025-04-28 02:10:40 +0100

    main page created

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\TrainerController;
 use App\Http\Controllers\GymClassController;
 use App\Http\Controllers\BookingController;
 use App\Http\Controllers\MembershipController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', [HomeController::class, 'index']);
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::get('/', function () {
-    return redirect()->route('classes.index');
-});
+require __DIR__.'/auth.php';
 
 Route::resource('users', UserController::class);
 Route::resource('trainers', TrainerController::class);
